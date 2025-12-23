52c6d0b50ef599c2d843c25842b9bf2f2bc91764
milanjana143 <mjana3321.important@gmail.com>
2025-12-23 20:49:39 +0530

    Rename mongo to mongodb in .env & db.js folder

--- aspyraController/config/db.js
+++ aspyraController/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require(`mongoose`);
 
 const connectdb = async() =>{
     try{
-        await mongoose.connect (process.env.MONGO_URI);
+        await mongoose.connect (process.env.MONGODB_URI);
         console.log(`Mogodb connected`);
     }
     catch(err){
